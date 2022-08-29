d65ea00cf05c00a59006ca309f11f87869bac29a
unknown <shadow97791@gmail.com>
2022-08-29 20:55:44 +0300

    Optimizations
    Now the initial fetch won't be on all items, but on the first 8 only (depending on the limit constant)

--- src/components/utils/Search.js
+++ src/components/utils/Search.js
@@ -3,20 +3,23 @@ import SearchField from "./SearchField";
 import constants from "./constants.js";
 import useFetch from "../../hooks/useFetch";
 
+const { limit } = constants;
+
 export default function Search({ setResponse }) {
     const [keywords, setKeywords] = useState("");
     const [offset, setOffset] = useState(0);
-    const [url, setUrl] = useState("rentals");
+    const [url, setUrl] = useState(
+        `rentals?page[limit]=${limit}&page[offset]=${offset}`
+    );
     const { data, isLoading, error } = useFetch(url);
-    const { limit } = constants;
 
     useEffect(() => {
-        setUrl(`rentals?page[limit]=${limit}&page[offset]=${offset}`);
-
         if (keywords) {
             setUrl(
                 `rentals?filter[keywords]=${keywords}&page[limit]=${limit}&page[offset]=${offset}`
             );
+        } else {
+            setUrl(`rentals?page[limit]=${limit}&page[offset]=${offset}`);
         }
 
         setResponse({ data: data?.data, isLoading, error });
