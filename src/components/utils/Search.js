b11c3cdcdd9ab666a0063a0ac67e9b8ab8cd7c09
unknown <shadow97791@gmail.com>
2022-08-29 19:28:31 +0300

    Search now sets an Object instead of an Array

--- src/components/utils/Search.js
+++ src/components/utils/Search.js
@@ -7,7 +7,7 @@ export default function Search({ setResponse }) {
     const [keywords, setKeywords] = useState("");
     const [offset, setOffset] = useState(0);
     const [url, setUrl] = useState("rentals");
-    const { data, loading, error } = useFetch(url);
+    const { data, isLoading, error } = useFetch(url);
     const { limit } = constants;
 
     useEffect(() => {
@@ -19,7 +19,7 @@ export default function Search({ setResponse }) {
             );
         }
 
-        setResponse([data?.data, loading, error]);
+        setResponse({ data: data?.data, isLoading, error });
     }, [keywords]);
 
     return (
