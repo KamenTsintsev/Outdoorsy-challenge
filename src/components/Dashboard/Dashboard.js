d6e90716a22f6b7d2e81817e3f7019f3af18671a
unknown <shadow97791@gmail.com>
2022-08-29 16:05:24 +0300

    Create Dashboard page

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+export default function Dashboard() {
+    const [data, setData] = useState([]);
+
+    return (
+        <>
+            <h1>Hii</h1>
+        </>
+    );
+}
