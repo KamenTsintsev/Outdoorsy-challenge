1ba81ed20e4addde50adbadb98d3530b2740919c
unknown <shadow97791@gmail.com>
2022-08-30 18:08:41 +0300

    Add styles for LoadMoreButton button

--- src/components/utils/LoadMoreButton.js
+++ src/components/utils/LoadMoreButton.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import constants from "./constants.js";
 
+import style from "./LoadMoreButton.module.scss";
+
 export default function LoadMoreButton({ setLimit }) {
     function onClickHandler() {
         setLimit((oldLimit) => oldLimit + constants.limit);
     }
 
-    return <button onClick={onClickHandler}>Load More...</button>;
+    return (
+        <button className={style.loadMoreBtn} onClick={onClickHandler}>
+            Load More...
+        </button>
+    );
 }
