0f4816531ea57f8540d9b03ddfc5acc7144e0db0
unknown <shadow97791@gmail.com>
2022-08-30 17:30:34 +0300

    add functionality for the Load More button

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,18 +1,32 @@
 import { useState, useEffect } from "react";
 import ProductList from "../ProductList/ProductList";
 import Search from "../utils/Search";
+import constants from "../utils/constants";
+import LoadMoreButton from "../utils/LoadMoreButton";
 
 import style from "./Dashboard.module.scss";
 
 export default function Dashboard() {
+    const [limit, setLimit] = useState(constants.limit);
     const [response, setResponse] = useState({});
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(response);
+    }, [response]);
+
     return (
         <div className={style.dashboardContainer}>
             <header>
-                <Search setResponse={setResponse} />
+                <Search setResponse={setResponse} limit={limit} />
             </header>
             <main>
-                <ProductList response={response} />
+                <ProductList response={data} />
+                {response?.data?.length > 0 ? (
+                    <LoadMoreButton setLimit={setLimit} />
+                ) : (
+                    ""
+                )}
             </main>
         </div>
     );
